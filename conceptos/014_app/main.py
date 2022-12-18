d7d1177f20077f3d00bc5543186b87ec46f2c15d
Marco Osorio <marco.osorio86@hotmail.com>
2022-12-17 20:37:06 -0500

    modificación de archivo main y creación de archivo example@

--- conceptos/014_app/main.py
+++ conceptos/014_app/main.py
@@ -1,18 +1,19 @@
 import mod
 
-keys, values = mod.get_population()
-print(keys, values)
+def run():
+    keys, values = mod.get_population()
+    print(keys, values)
 
-data = [
-    {
-        'country' : 'Colombia',
-        'Population' : 500
-    },
-    {
-        'country' : 'Bolivia',
-        'Population' : 300
-    }
-]
-country = input('Ingresa el país que deseas consultar:  ')  # Se debe escribir el nombre bien para que se pueda realizar la consulta
-result = mod.population_by_country(data, country)
-print(result)
+    data = [
+        {
+            'country' : 'Colombia',
+            'Population' : 500
+        },
+        {
+            'country' : 'Bolivia',
+            'Population' : 300
+        }
+    ]
+    country = input('Ingresa el país que deseas consultar:  ')  # Se debe escribir el nombre bien para que se pueda realizar la consulta
+    result = mod.population_by_country(data, country)
+    print(result)
